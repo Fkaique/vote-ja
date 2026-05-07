68fb3476a92512e7a1ea43dbd88eab0a25dde672
Fkaique <fkaiquesp@gmail.com>
2026-05-07 17:49:08 -0300

    feat: teclado da urna

---
+++ src/Pages/Simulator.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Urna from '../components/Urna/Urna.vue';
+
+const numeroDigitado = ref('');
+
+function handleDigit(n: number) {
+    if (numeroDigitado.value.length < 5) {
+        numeroDigitado.value += n.toString();
+    }
+}
+
+</script>
+
+<template>
+    <div class="containerUrna">
+        <div class="painel">
+            <div class="display-numero">
+                {{ numeroDigitado }}
+            </div>
+        </div>
+        <div class="urna">
+            <Urna @digit-pressed="handleDigit" />
+        </div>
+    </div>
+</template>
+
+<style>
+
+.containerUrna {
+    display: flex;
+}
+
+.urna {
+    display: flex;
+    width: 500px;
+    height: 500px;
+    background-color: var(--color-text);
+    padding: 40px 60px;
+    font-size: 3rem;
+}
+
+.painel {
+    width: 300px;
+    height: 100px;
+    background-color: #f0f0f0;
+    border: 2px solid #000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 2rem;
+}
+</style>
---
+++ src/components/Urna/ButtonNumber.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 
+const emit = defineEmits(['clickDigit']);
+const props = defineProps<{
+    digit: Digit
+}>()
+
+</script>
+
+<template>
+    <button class="btnUrna" @click="emit('clickDigit', props.digit)">
+        {{ digit }}
+    </button>
+</template>
+
+<style>
+
+.btnUrna {
+    display: flex;
+    justify-content: start;
+    align-items: start;
+    background-color: var(--color-preto);
+    color: var(--color-branco);
+    width: 100%;
+    height: 100%;
+}
+
+</style>
